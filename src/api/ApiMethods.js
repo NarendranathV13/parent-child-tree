2b2f621ab2be660fe58bf0d9d6ff5fead23f5c66
NarendranathV13 <narendranath.v@arkinfotec.com>
2023-10-04 19:30:18 +0530

    modified ui and added child to parent check

--- src/api/ApiMethods.js
+++ src/api/ApiMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `https://651be895194f77f2a5af1087.mockapi.io/camera`,
+    baseURL: `https://651be895194f77f2a5af1087.mockapi.io`,
 });
   const GetAxiosData = (apiUrl) => {
     return api.get(apiUrl);
